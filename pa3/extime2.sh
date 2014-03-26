a8aafdb24d851816bb23c15336a0b2eabb605aaa
Vince Coghlan <fatty@ubuntu.(none)>
2014-03-26 12:18:56 -0700

    bfs tests

---
+++ pa3/extime2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo '\begin{center}'
+echo '\begin{tabular}{|c|c|c|c|c|}'
+echo '\hline'
+echo 'Schedule & Utilization & Type & Trial & Execution Time\\'
+echo '\hline'
+echo '\hline'
+
+
+for sched in SCHED_RR
+do
+unset num
+unset temp
+for typ in mixed
+do
+echo -ne $sched
+echo -ne ' & High & '
+echo -ne $typ
+echo -ne ' & 1 & '
+num=`sudo ./pa3 250 $sched $typ extime`
+echo $num '\\'
+echo '\cline{4-5}'
+for ((i=0; i<9; i++))
+do
+temp=`sudo ./pa3 250 $sched $typ extime`
+echo ' & & &' $((i+2)) '&' $temp '\\'
+echo '\cline{4-5}'
+num=`echo $num`+`echo $temp`
+done
+temp=`echo 'scale=6; ('$num')/10' | bc -l`
+echo ' & & & avg &' $temp '\\'
+echo '\hline'
+done
+
+done
+
+echo '\end{tabular}'
+echo '\end{center}'
